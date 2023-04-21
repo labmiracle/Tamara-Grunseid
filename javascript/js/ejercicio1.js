55b7b633e7d650fdbb34664a4d1857b94ebf9144
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-04-21 10:02:05 -0300

    agregué Ejercicio 1

---
+++ javascript/js/ejercicio1.js
@@ -0,0 +1,20 @@
+function golfScore (par, strokes) { 
+    if (strokes==1) {
+        return "Hole-in-one!";
+    } else if (strokes<=par-2) {
+            return "Eagle";
+        } else if (strokes==par-1) {
+                return "Birdie";
+            } else if (strokes==par) {
+                    return "Par";
+                } else if (strokes==par+1) {
+                        return "Bogey";
+                    } else if (strokes==par+2) {
+                            return "Double Bogey";
+                        } else if (strokes>=par+3) {
+                                return "Go Home";
+                            }}
+
+         
+
+
