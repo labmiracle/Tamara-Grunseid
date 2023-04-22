0535e76610565fe57da8d6815c1cb7f3ee9ec2e2
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-04-22 10:34:04 -0300

    resolución parte B ejercicio 4

--- javascript/js/ejercicio4.js
+++ javascript/js/ejercicio4.js
@@ -8,3 +8,13 @@ function palabraMasLarga(str) {
     }
     return compara; 
  }
+
+ function primeraMayuscula (cadena) {
+    let cadenaVector = cadena.split(" ");
+    vectorFinal = new Array (cadenaVector.length);
+    for (let j = 0; j < cadenaVector.length; j++) {
+        vectorFinal[j] = cadenaVector[j].charAt(0).toUpperCase()+cadenaVector[j].slice(1)
+    }
+   return vectorFinal.join(" ");
+ }
+ 
