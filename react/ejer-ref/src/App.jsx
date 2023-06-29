39ec2fcf42afff90ad90d126e9891ca6ad8490d4
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-06-29 10:35:36 -0300

    resolucion de ejercicio rest

--- react/ejer-ref/src/App.jsx
+++ react/ejer-ref/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Form from "./components/form";
 
 function App() {
   return (
     <>
       <h1>Ejercicio rest</h1>
+      <Form> </Form>
     </>
   );
 }
--- react/ejer-ref/src/components/Form.jsx
+++ react/ejer-ref/src/components/Form.jsx
@@ -1,1 +1,46 @@
-export default function Form() {}
+import { useState } from "react";
+
+export default function Form() {
+  const [valorInput1, setValorInput1] = useState("");
+  const [valorInput2, setValorInput2] = useState("");
+
+  const handleInputChange1 = (event) => {
+    setValorInput1(event.target.value);
+  };
+
+  const handleInputChange2 = (event) => {
+    setValorInput2(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Valor del Input 1:", valorInput1);
+    console.log("Valor del Input 2:", valorInput2);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        {" "}
+        Input 1:{" "}
+        <input
+          type="text"
+          value={valorInput1}
+          onChange={handleInputChange1}
+        />{" "}
+      </label>
+      <br />
+      <label>
+        {" "}
+        Input 2:{" "}
+        <input
+          type="text"
+          value={valorInput2}
+          onChange={handleInputChange2}
+        />{" "}
+      </label>
+      <br />
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
