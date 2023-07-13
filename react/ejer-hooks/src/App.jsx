3753b9b22e2f4cc19e8a0817bd00871824501c8f
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-07-13 10:04:04 -0300

    creacion del componente Desmontado

--- react/ejer-hooks/src/App.jsx
+++ react/ejer-hooks/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import AforB from "./components/AforB";
+import Desmontado from "./components/Desmontado";
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <p> 1) Cambia a por b</p>
       <AforB></AforB>
       <br />
+      <p> 2) Desmontado se ve en la terminal</p>
+      <Desmontado></Desmontado>
     </>
   );
 }
---
+++ react/ejer-hooks/src/components/Desmontado.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+export default function Desmontado() {
+  useEffect(() => {
+    return () => {
+      console.log("Desmontado!");
+    };
+  }, []);
+
+  return null;
+}
