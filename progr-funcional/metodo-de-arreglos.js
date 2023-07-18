1126820dcdde4c6eae56e1bc3ffb9f3cca51928c
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-07-18 10:58:01 -0300

    resolucion funcion map, 1 y 2 de funcion reduce

---
+++ progr-funcional/metodo-de-arreglos.js
@@ -0,0 +1,63 @@
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
+];
+
+//FUNCION MAP
+// 1. Obtener un arreglo de todos los nombres
+const names = characters.map((character) => character.name);
+console.log("FUNCION MAP 1.", names);
+
+// 2. Obtener un arreglo de todas las alturas
+const heights = characters.map((character) => character.height);
+console.log("FUNCION MAP 2", heights);
+
+// 3. Obtener un arreglo de objetos con las propiedades de nombre y altura
+const nameHeightPairs = characters.map((character) => ({
+  name: character.name,
+  height: character.height,
+}));
+console.log("FUNCION MAP 3.", nameHeightPairs);
+
+// 4. Obtener un arreglo de todos los nombres
+console.log("FUNCION MAP 4.", names);
+
+// FUNCION REDUCE
+
+// 1. Obtener la masa total de los personajes
+const totalMass = characters
+  .map((character) => parseInt(character.mass))
+  .reduce((acc, curr) => acc + curr, 0);
+console.log("FUNCION REDUCE 1.", totalMass);
+
+//// 2. Obtener la altura total de todos los caracteres
+const totalHeight = characters
+  .map((character) => parseInt(character.height))
+  .reduce((acc, curr) => acc + curr, 0);
+console.log("FUNCION REDUCE 2.", totalHeight);
