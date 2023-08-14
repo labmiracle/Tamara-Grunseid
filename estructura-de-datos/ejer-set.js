ef623822c704bda3360247cef9f0353476d9d30d
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-08-14 12:46:29 -0300

    resolucion ejercicio 2 de set

--- estructura-de-datos/ejer-set.js
+++ estructura-de-datos/ejer-set.js
@@ -9,3 +9,12 @@ console.log(isDuplicated([1, 2, 3, 4, 5, 6, 7]));
 console.log(isDuplicated([1, 2, 3, 4, 4, 6, 7]));
 
 //EJERCICIO 2
+function uniqueElemArr(str1, str2) {
+  const setExample2 = new Set([...str1, ...str2]);
+  return Array.from(setExample2);
+}
+
+const str1 = [1, 2, 3, 4, 5, 6, 7, 2];
+const str2 = [3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(uniqueElemArr(str1, str2));
