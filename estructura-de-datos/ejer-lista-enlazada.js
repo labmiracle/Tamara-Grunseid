d5931b6b837df71863af28259a372c3d388ceb5e
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-08-16 11:39:53 -0300

    definicion de metodo add

---
+++ estructura-de-datos/ejer-lista-enlazada.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  add(data) {
+    let node = new Node(data);
+    let current;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+
+    this.size++;
+  }
+}
