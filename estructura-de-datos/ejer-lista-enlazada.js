50381d4bf9007131a890e6ac19d9b17d297c90bb
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-08-17 12:16:15 -0300

    definicion metodo invert

--- estructura-de-datos/ejer-lista-enlazada.js
+++ estructura-de-datos/ejer-lista-enlazada.js
@@ -67,4 +67,19 @@ class LinkedList {
     }
     return null;
   }
+
+  invert() {
+    let prev = null;
+    let current = this.head;
+    let next = null;
+
+    while (current) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    this.head = prev;
+  }
 }
